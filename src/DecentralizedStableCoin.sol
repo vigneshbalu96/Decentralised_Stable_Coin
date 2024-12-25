e110a5ac21cc4c1828eb620721430836f49c868c
Vignesh Balusamy <vigneshbalu96@gmail.com>
2024-12-25 08:24:09 +0530

    First Commit

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Vignesh Balusamy
+ * Minting: Algorithmic
+ * Collateral: Exogenous (Eth & BTC)
+ * Relative Stability: Pegged to USD
+ *
+ * @notice This contract is an ERC20 implementation and meant to be governed by DSCEngine.
+ */
+
+contract DecentralizedStableCoin {
+    constructor() {}
+}
