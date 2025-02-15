99cad95cb3bd8159d795d3feb2afdc36cfee55ec
Vignesh Balusamy <vigneshbalu96@gmail.com>
2025-02-15 09:37:48 +0530

    Burnable contract added

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,15 +1,54 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.20;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title DecentralizedStableCoin
  * @author Vignesh Balusamy
  * Minting: Algorithmic
- * Collateral: Exogenous (Eth & BTC)
+ * Collateral: Exogenous (Ether & Bitcoin)
  * Relative Stability: Pegged to USD
  *
- * @notice This contract is an ERC20 implementation of our stable_coin system and it is meant to be governed by DSCEngine.
+ * @notice This contract is an ERC20 implementation of our stable_coin system and it is meant to be governed by DSCEngine. This contract is just the ERC-20 implementation of our stablecoin system.
  */
-contract DecentralizedStableCoin {
-    constructor() {}
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BalanceMustBeMoreThanZero();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance <= _amount) {
+            revert DecentralizedStableCoin__BalanceMustBeMoreThanZero();
+        }
+    }
 }
