98b16fb5638954af4bb76243e0c2a01650627bed
Vignesh Balusamy <vigneshbalu96@gmail.com>
2025-02-15 13:23:26 +0530

    Functions added

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -26,4 +26,33 @@ pragma solidity ^0.8.20;
 
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 
-contract DSCEngine {}
+/**
+ * @title DSC Engine
+ * @author Vignesh Balusamy
+ * @notice This system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $ 1 peg.
+ * @notice This Stablecoin has the following properties -
+ * 1. Dollar pegged
+ * 2. Exogenous Collateral
+ * 3. Algorithmically stable
+ * Our DSC system is always wanted to be "Overcollateralized", at no point the value of all collateral <= the $ backed value of all the DSC
+ * @notice It is similar to DAI, if DAI had no governance, no fees and was only backed by wETH & wBTC.
+ * @notice This contract is the core of the DSC system. It handles all the logic for minting and redeeming DSC, as well as depositing and withdrawing crypto collateral
+ * @notice This contract is very loosely based on the MakerDAO DSS (DAI) system.
+ */
+contract DSCEngine {
+    function depositCollateralAndMintDSC() external {}
+
+    function depositCollateral() external {}
+
+    function redeemCollateralForDSC() external {}
+
+    function redeemcollateral() external {}
+
+    function mintDSC() external {}
+
+    function burnDSC() external {}
+
+    function getHealthFactor() external {}
+
+    function liquidate() external {}
+}
